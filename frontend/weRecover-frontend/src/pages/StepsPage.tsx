820900d978e36a73d074ce93cce70f9f6f2077df
Alexander Sarnov <alexander.sarnov@gmail.com>
2024-11-19 21:26:41 +0200

    Add footer to StepsPage

--- frontend/weRecover-frontend/src/pages/StepsPage.tsx
+++ frontend/weRecover-frontend/src/pages/StepsPage.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import StepList from '../components/StepList';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 import '../styles/StepsPage.css'; // Updated import path
 
 const StepsPage: React.FC = () => {
     return (
-        <div>
+        <div className="d-flex flex-column min-vh-100">
             <Header />
-            <div className="steps-page-container">
+            <div className="container my-auto steps-page-container">
                 <div className="steps-page-welcome">
                     <h1>Welcome to Your Recovery Journey</h1>
                     <p>Your path to recovery starts here. Explore the steps and progress at your own pace.</p>
                 </div>
                 <StepList />
             </div>
+            <Footer />
         </div>
     );
 };
