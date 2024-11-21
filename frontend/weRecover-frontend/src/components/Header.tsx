a45d442927740427979c6d7cdde724a91546ba5a
Alexander Sarnov <alexander.sarnov@gmail.com>
2024-11-21 23:37:07 +0200

    Finalized design updates

--- frontend/weRecover-frontend/src/components/Header.tsx
+++ frontend/weRecover-frontend/src/components/Header.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { logout } from '../services/authService';
