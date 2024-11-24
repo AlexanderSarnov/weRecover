a1a7acdec2682844bf3c6024e24383c99ce269c2
Alexander Sarnov <alexander.sarnov@gmail.com>
2024-11-24 16:18:28 +0200

    Updated styles and added micro changes to functionality

--- frontend/weRecover-frontend/src/components/Header.tsx
+++ frontend/weRecover-frontend/src/components/Header.tsx
@@ -1,22 +1,23 @@
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { logout } from '../services/authService';
 import logo from '../assets/logo.png';
 import userAvatar from '../assets/userAvatar.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Ensure Bootstrap JS is included
 
 const Header = () => {
     return (
-        <Navbar bg="light" expand="lg">
-            <div className="container">
+        <Navbar bg="light" expand="lg" className="w-100">
+            <Container fluid>
                 <LinkContainer to="/">
-                    <Navbar.Brand>
+                    <Navbar.Brand className="d-flex align-items-center">
                         <img src={logo} alt="weRecover Logo" className="img-fluid" style={{ height: '50px' }} />
                     </Navbar.Brand>
                 </LinkContainer>
                 <Navbar.Toggle aria-controls="navbarNav" />
-                <Navbar.Collapse id="navbarNav">
-                    <Nav className="ml-auto">
+                <Navbar.Collapse id="navbarNav" className="justify-content-between">
+                    <Nav className="mr-auto">
                         <LinkContainer to="/">
                             <Nav.Link>Home</Nav.Link>
                         </LinkContainer>
@@ -29,6 +30,8 @@ const Header = () => {
                         <LinkContainer to="/settings">
                             <Nav.Link>Settings</Nav.Link>
                         </LinkContainer>
+                    </Nav>
+                    <Nav>
                         <NavDropdown
                             title={
                                 <img
@@ -46,7 +49,7 @@ const Header = () => {
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
-            </div>
+            </Container>
         </Navbar>
     );
 };
